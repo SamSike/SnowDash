af4f5cefd39145a8a94f7cd0e29947b929cefe03
Sameer Sikka <sameersikka7@gmail.com>
2022-10-14 22:58:15 +1100

    Character movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,62 @@ using UnityEngine;
 using UnityEngine.Playables;
 
 [System.Serializable]
-public class Player : PlayableAsset
+public class Player : PlayableAsset, MonoBehaviour
 {
+    // private bool isMovingX = false;
+    // private bool isMovingY = false;
+    // private int[] possiblePositionsX = {-5, 0, 5};
+    // private Vector3 moveDirection;
+    // private float xMoveTime = 3; // in seconds
+    // private float yMoveTime = 3; // in seconds
+    
+
     // Factory method that generates a playable based on this asset
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
     {
         return Playable.Create(graph);
+    }
+
+    private void Update(){
+        if(Input.GetAxis("Horizontal") > 0){
+            if(this.position.x > 0)
+                RightEdgeHit();
+            else
+                MoveRight();
+        }
+        if(Input.GetAxis("Horizontal") < 0){
+            if(this.position.x < 0)
+                LeftEdgeHit();
+            else 
+                MoveLeft();
+        }
+        if(Input.GetAxis("Vertical") > 0){
+            Jump();
+        }
+        if(Input.GetAxis("Vertical") < 0){
+            Duck();
+        }
+    }
+
+    private void FixedUpdate(){
+        // if (isMovingX && possiblePositionsX.Contains(this.position.x)){
+        //     isMovingX = false;
+        //     moveDirection = Vector3.zero;
+        // }
+        // if (isMoving)
+        //     this.position += moveDirection / (moveTime * Time.fixedDeltaTime);
+    }
+
+    private void MoveRight(){
+        isMoving = true;
+    }
+    private void MoveLeft(){
+        isMoving = true;
+    }
+    private void Jump(){
+        isMoving = true;
+    }
+    private void Duck(){
+        isMoving = true;
     }
 }
