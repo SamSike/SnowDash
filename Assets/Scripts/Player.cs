ce59d1f38545d538c0473c6b8db519d05a41e51f
Sameer Sikka <sameersikka7@gmail.com>
2022-10-14 23:11:47 +1100

    error fixing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Playables;
 
 [System.Serializable]
-public class Player : PlayableAsset, MonoBehaviour
+public class Player : MonoBehaviour
 {
     // private bool isMovingX = false;
     // private bool isMovingY = false;
@@ -12,23 +11,18 @@ public class Player : PlayableAsset, MonoBehaviour
     // private Vector3 moveDirection;
     // private float xMoveTime = 3; // in seconds
     // private float yMoveTime = 3; // in seconds
+    private Vector3 xMove = new Vector3(5,0,0);
+    private Vector3 yMove = new Vector3(0,5,0);
     
-
-    // Factory method that generates a playable based on this asset
-    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
-    {
-        return Playable.Create(graph);
-    }
-
     private void Update(){
         if(Input.GetAxis("Horizontal") > 0){
-            if(this.position.x > 0)
+            if(this.transform.position.x > 0)
                 RightEdgeHit();
             else
                 MoveRight();
         }
         if(Input.GetAxis("Horizontal") < 0){
-            if(this.position.x < 0)
+            if(this.transform.position.x < 0)
                 LeftEdgeHit();
             else 
                 MoveLeft();
@@ -51,15 +45,27 @@ public class Player : PlayableAsset, MonoBehaviour
     }
 
     private void MoveRight(){
-        isMoving = true;
+        // isMoving = true;
+        this.transform.position += xMove;
     }
     private void MoveLeft(){
-        isMoving = true;
+        // isMoving = true;
+        this.transform.position -= xMove;
     }
     private void Jump(){
-        isMoving = true;
+        // isMoving = true;
+        this.transform.position += yMove;
     }
     private void Duck(){
-        isMoving = true;
+        // isMoving = true;
+        this.transform.position -= yMove;
+    }
+
+    private void LeftEdgeHit(){
+        
+    }
+
+    private void RightEdgeHit(){
+
     }
 }
