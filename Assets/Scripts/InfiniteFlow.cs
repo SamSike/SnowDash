b2a0453ec2e15d3fc2b50f79c39db31fab72cfe1
Jason Phan <jp12300234@gmail.com>
2022-10-16 22:40:59 +1100

    obstacles spawning

--- Assets/Scripts/InfiniteFlow.cs
+++ Assets/Scripts/InfiniteFlow.cs
@@ -6,11 +6,22 @@ public class InfiniteFlow : MonoBehaviour
 {
     public Transform tile1;
     private Vector3 nextTileSpawn;
+    public Transform tree1;
+    private Vector3 nextObstV;
+    public Transform treeStump1;
+
+    private List<Transform> obstacles;
+    private int randX;
+
     // Start is called before the first frame update
     void Start()
     {
+        obstacles = new List<Transform>();
         nextTileSpawn.z = 60;
+        obstacles.Add(tree1);
+        obstacles.Add(treeStump1);
         StartCoroutine(spawnTile());
+
     }
 
     // Update is called once per frame
@@ -21,8 +32,17 @@ public class InfiniteFlow : MonoBehaviour
     IEnumerator spawnTile()
     {
         yield return new WaitForSeconds(2);
+        randX = Random.Range(-1, 2);
+
         Instantiate(tile1, nextTileSpawn, tile1.rotation);
+        nextObstV = nextTileSpawn;
+        nextObstV.x = randX;
+        Transform obst = obstacles[Random.Range(0, obstacles.Count)];
+        Instantiate(obst, nextObstV, obst.rotation);
+
         nextTileSpawn.z += 20;
         StartCoroutine(spawnTile());
+
+
     }
 }
