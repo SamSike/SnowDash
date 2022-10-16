4da1363c686074e7dc6b8424f0bc7ba26497f268
Jason Phan <jp12300234@gmail.com>
2022-10-16 23:52:50 +1100

    branch obstacles

--- Assets/Scripts/InfiniteFlow.cs
+++ Assets/Scripts/InfiniteFlow.cs
@@ -6,20 +6,25 @@ public class InfiniteFlow : MonoBehaviour
 {
     public Transform tile1;
     private Vector3 nextTileSpawn;
-    public Transform tree1;
     private Vector3 nextObstV;
+    public Transform tree1;
+    public Transform branch1;
     public Transform treeStump1;
 
     private List<Transform> obstacles;
+    private List<Transform> edgeObstacles;
     private int randX;
 
     // Start is called before the first frame update
     void Start()
     {
         obstacles = new List<Transform>();
+        edgeObstacles = new List<Transform>();
         nextTileSpawn.z = 60;
         obstacles.Add(tree1);
         obstacles.Add(treeStump1);
+        edgeObstacles.AddRange(obstacles);
+        edgeObstacles.Add(branch1);
         StartCoroutine(spawnTile());
 
     }
@@ -37,12 +42,21 @@ public class InfiniteFlow : MonoBehaviour
         Instantiate(tile1, nextTileSpawn, tile1.rotation);
         nextObstV = nextTileSpawn;
         nextObstV.x = randX;
-        Transform obst = obstacles[Random.Range(0, obstacles.Count)];
+        Transform obst;
+
+        if (randX == -1 || randX == 1)
+        {
+            obst = edgeObstacles[Random.Range(0, edgeObstacles.Count)];
+        }
+        else
+        {
+            obst = obstacles[Random.Range(0, obstacles.Count)];
+        }
+
+        if (obst == branch1) nextObstV.y = 0.75f;
         Instantiate(obst, nextObstV, obst.rotation);
 
         nextTileSpawn.z += 20;
         StartCoroutine(spawnTile());
-
-
     }
 }
