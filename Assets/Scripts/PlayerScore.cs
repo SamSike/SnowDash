d2c8dee84c9b3d6b7baaea53b701bddf56ec971e
xukoikok <kokx@student.unimelb.edu.au>
2022-10-17 17:24:44 +1100

    bug fix on score UI

--- Assets/Scripts/PlayerScore.cs
+++ Assets/Scripts/PlayerScore.cs
@@ -5,6 +5,7 @@ public class PlayerScore : MonoBehaviour
 {
     public Transform player;
     public Text scoreText;
+    public Text highScoretext;
     void Update()
     {
         scoreText.text = "SCORE: "+ player.position.z.ToString("0");
