ec6298b03cdae0ebcc78aefa956b27a4dd173b53
rebegul <109938265+reguevara@users.noreply.github.com>
2022-10-30 23:49:15 +1100

    Camera following

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,23 +1,28 @@
+
 using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform player;
-    Vector3 offset;
-    float origY;
+    //Vector3 offset;
+    public Vector3 offset;
+    /*float origY;
     private void Start()
     {
-        offset = transform.position - player.position;
-        origY = transform.position.y;
+        //offset = transform.position - player.position;
+        //origY = transform.position.y;
+       
+        //offset.(0.0,2.27,-5.02);
 
-
-    }
+    }*/
 
     private void Update()
     {
-        Vector3 targetPos = player.position + offset;
+       /* Vector3 targetPos = player.position + offset;
         targetPos.x = 0;
-        targetPos.y = origY;
-        transform.position = targetPos;
+        targetPos.y = origY;*/
+        //transform.position = targetPos;
+        
+        transform.position = player.position + offset;
     }
 
 
